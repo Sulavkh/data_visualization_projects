fc295aa9b98165054fe6921129bc6f51cf751773
Sulavkh <sulove.kh@gmail.com>
2024-09-18 20:49:21 +0545

    two dices and dice with 6 and 10 sides

--- dice/dice_visual.py
+++ dice/dice_visual.py
@@ -1,23 +1,25 @@
 import plotly.express as px
 from die import Die
-#create a D6.
-die = Die()
+#create two Dice.
+die_1 = Die()
+die_2 = Die()
 
 #make some rolls and store results in a list.
 results = []
 for roll_numn in range(1000):
-    result =die.roll()
+    result =die_1.roll() + die_2.roll()
     results.append(result)
 
 #analyze the results.
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(1, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 #visualize the results.
-title = "Results of rolling one Dice 1000 times."
+title = "Results of rolling two Dice 1000 times."
 labels = {'x':'Result', 'y':'Frequency of Result'} #defining axix labels as dictionary
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.show()
--- dice/dice_visuald6d10.py
+++ dice/dice_visuald6d10.py
@@ -1,23 +1,25 @@
 import plotly.express as px
 from die import Die
-#create a D6.
-die = Die()
+#create two Dice d6 and d10.
+die_1 = Die()
+die_2 = Die(10)
 
 #make some rolls and store results in a list.
 results = []
-for roll_numn in range(1000):
-    result =die.roll()
+for roll_numn in range(50000):
+    result =die_1.roll() + die_2.roll()
     results.append(result)
 
 #analyze the results.
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(1, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 #visualize the results.
-title = "Results of rolling one Dice 1000 times."
+title = "Results of rolling two Dice 50000 times."
 labels = {'x':'Result', 'y':'Frequency of Result'} #defining axix labels as dictionary
 fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 fig.show()
