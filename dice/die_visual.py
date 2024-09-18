e54ba1b67afe3b508f6b9c9fce34998f5e23ad9f
Sulavkh <sulove.kh@gmail.com>
2024-09-18 19:14:22 +0545

    added frequency of numbers counted

--- dice/die_visual.py
+++ dice/die_visual.py
@@ -1,11 +1,21 @@
+import plotly.express as px
 from die import Die
-
 #create a D6.
 die = Die()
 
 #make some rolls and store results in a list.
 results = []
-for roll_numn in range(100):
+for roll_numn in range(1000):
     result =die.roll()
     results.append(result)
-print(results)
+
+#analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#visualize the results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
